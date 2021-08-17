bea8beb98acea9a48a42a8f609ba847c59c875f2
Marisha <75089445+MarizzaM@users.noreply.github.com>
2021-08-17 14:24:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const http = require("https");
+// const http = require("https");
 const fs = require("fs");
 
-const url = "https://gist.githubusercontent.com/keeguon/2310008/raw/bdc2ce1c1e3f28f9cab5b4393c7549f38361be4e/countries.json";
+// const url = "https://gist.githubusercontent.com/keeguon/2310008/raw/bdc2ce1c1e3f28f9cab5b4393c7549f38361be4e/countries.json";
 
-http.get(url, function(res){
-    const fileStream = fs.createWriteStream("countries.json")
-    res.pipe(fileStream);
-    fileStream.on("finish", function(){
-        fileStream.close();
-        console.log("Done!");
-    });
-});
+// http.get(url, function(res){
+//     const fileStream = fs.createWriteStream("countries.json")
+//     res.pipe(fileStream);
+//     fileStream.on("finish", function(){
+//         fileStream.close();
+//         console.log("Done!");
+//     });
+// });
 
-const content = fs.readFileSync('countries.json', 'utf-8');
 
-console.log(content);
+fs.readFile('countries_1.json', 'utf8',(err, fileContent) => {
+    if( err ) {
+    } else {
+        let  data = JSON.parse(fileContent.toString());
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i]);
+          }
+    }
+})
